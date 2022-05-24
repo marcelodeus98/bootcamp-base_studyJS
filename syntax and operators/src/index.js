fd6f779441666fe6033f05f1f91bb2738dbd6df6
Jose helio <helio@COLMEIA.LOCAL>
2022-05-24 10:16:13 -0300

    challenge submission syntax and operators

---
+++ syntax and operators/src/index.js
@@ -0,0 +1,20 @@
+let sum = ( number_one, number_two) => {
+    number_one = parseInt(prompt("Enter a number one"));
+    number_two = parseInt(prompt("Enter a number two"));
+    let valid_number_equals;
+    let total = number_one + number_two;
+    let total_is_one;
+    let total_is_two;   
+    
+    number_one === number_two ? valid_number_equals = 'The numbers are same!' : valid_number_equals = 'The numbers are different!';
+  
+
+    total > 10 ? total_is_one = 'is bigger than 10' : total_is_one = 'is smalller than 10';
+
+    total > 20 ? total_is_two = 'is bigger than 20' : total_is_two = 'is smaller than 20';
+    
+    alert(`${valid_number_equals}.The sum of numbers is ${total}, what is ${total_is_one}, and ${total_is_two}`)
+
+  }
+
+  sum()
